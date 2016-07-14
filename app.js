6eafd55f1f949d47c960d10a9c695859c3a7c77c
pazmarie <nicaygwapa@gmail.com>
2016-07-14 19:03:13 +0800

    MODULESSAM

---
+++ app.js
@@ -0,0 +1,9 @@
+const util = require('./mymodule');
+
+var area = util.computeArea(10,50);
+
+console.log ("area is:" +area);
+
+var perimeter = util.computePerimeter(10,50);
+
+console.log ("perimeter is:" +perimeter);
---
+++ mymodule.js
@@ -0,0 +1,16 @@
+
+
+function area(length,width){
+    return length * width;
+}
+
+function perimeter(length, width){
+    return (length+width) * 2;
+   
+}
+
+module.exports = {
+    computeArea: area,
+    computePerimeter: perimeter
+    
+};
